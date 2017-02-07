a910238c15bc0d639d5053d14e9c24a78cd92f83
Yuval Allweil <themarquisdesheric@gmail.com>
2017-02-07 15:12:50 -0800

    Added third question, along with inconsequential formatting

--- app.js
+++ app.js
@@ -3,10 +3,7 @@
 //maybe add a while loop to these for if they provided an invalid response?
 
 var firstAnswer = prompt('Is Yuval an Israeli citizen?', 'Please enter "yes" or "no"').toLowerCase();
-
-console.log('Asked user if Yuval is an Israeli citizen (true).');
-
-
+console.log('1) Asked user if Yuval is an Israeli citizen (true).');
 
 if (firstAnswer === 'yes' || firstAnswer === 'y') {
   confirm('You are correct! Yuval has dual citizenship :)');
@@ -19,12 +16,13 @@ if (firstAnswer === 'yes' || firstAnswer === 'y') {
   console.log('User responded unintelligbly! For shame.');
 }
 
-var secondAnswer = prompt('Does Yuval have a degree from a higher learning institution?', 'Please enter "yes" or "no"').toLowerCase();
+console.log('\n'); 
 
-console.log('Asked user if Yuval has a degree from a higher learning institution (false).');
+var secondAnswer = prompt('Does Yuval have a degree from a higher learning institution?', 'Please enter "yes" or "no"').toLowerCase();
+console.log('2) Asked user if Yuval has a degree from a higher learning institution (false).');
 
 if (secondAnswer === 'yes' || secondAnswer === 'y') {
-  confirm('That is incorrect (but you made me blush, if that helps). Libraries and the internet are free!');
+  confirm('That is incorrect (but you made me blush, if that helps) Libraries and the internet are free!');
   console.log('User answered incorrectly!');
 } else if (secondAnswer === 'no' || secondAnswer === 'n') {
   confirm('That is correct! (much to the chagrin of my parents)');
@@ -34,10 +32,25 @@ if (secondAnswer === 'yes' || secondAnswer === 'y') {
   console.log('User responded unintelligbly! For shame.');
 }
 
+console.log('\n'); 
+
+var thirdAnswer = prompt('Has Yuval been to the magical land of India?', 'Please enter "yes" or "no"').toLowerCase();
+console.log('3) Asked user if Yuval has been to India (true).');
+
+if (thirdAnswer === 'yes' || thirdAnswer === 'y') {
+  confirm('That is correct! Several times -- including an illegal border crossing...');
+  console.log('User answered correctly!');
+} else if (thirdAnswer === 'no' || thirdAnswer === 'n') {
+  confirm('That is incorrect. Yuval is quite the cosmopolitan mofo ;)');
+  console.log('User answered incorrectly!');
+} else {
+  confirm('Please enter a valid "yes" or "no" response.');
+  console.log('User responded unintelligbly! For shame.');
+}
+
+console.log('\n'); 
 
 
-
-//has yuval been to india? 
 
 //are there two of yuval?
 
